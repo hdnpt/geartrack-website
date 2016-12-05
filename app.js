d5b6391356cf70ad1a91c0f3f9dde984aa089bfc
Carlos Florencio <carlosmflorencio@gmail.com>
2016-12-05 00:42:36 +0000

    Added year to 404 page

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/api', api)
 app.use(function(req, res, next) {
   const err = new Error('Not Found')
   err.status = 404
-  res.status(404).render('404', {title: "404 - Geartrack"})
+  res.status(404).render('404', {title: "404 - Geartrack", year: new Date().getFullYear() })
 })
 
 /*
